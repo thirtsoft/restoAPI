f56fb70738464deb2e18cc0b9fbd6577b2849414
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:46:33 +0000

    first commit

---
+++ src/main/java/com/resto/RestoApiApplication.java
@@ -0,0 +1,13 @@
+package com.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resto/RestoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
